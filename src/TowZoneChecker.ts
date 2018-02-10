dea5f36f48e088c4cf4dc214cd354c45fe000466
Evan Seguin <seguin.evan@gmail.com>
2018-02-10 09:54:46 -0800

    Switch to a new sfgov dataset
    The old one was deleted / hidden :(

--- src/TowZoneChecker.ts
+++ src/TowZoneChecker.ts
@@ -5,13 +5,13 @@ import * as moment from 'moment';
 import { IAddressConfig } from './ConfigWrapper';
 
 interface ISFGovTowZone {
-  toaddress: string;
-  fromaddress: string;
-  streetname: string;
-  starting_date: string; // eslint-disable-line
-  ending_date: string; // eslint-disable-line
+  address: string;
+  sideofstreet: string;
+  startdate: string;
+  enddate: string;
   starttime: string;
   endtime: string;
+  notes: string;
 }
 
 export interface ITowZone {
@@ -20,36 +20,36 @@ export interface ITowZone {
 }
 
 const parseRow = (row: ISFGovTowZone) => {
-  const addressRange = `${row.fromaddress} - ${row.toaddress} ${row.streetname}`;
-  const startDate = moment(row.starting_date).format('dddd, MMMM Do YYYY');
-  const endDate = moment(row.ending_date).format('dddd, MMMM Do YYYY');
+  const startDate = moment(row.startdate, 'MM/DD/YYYY').format('dddd, MMMM Do YYYY');
+  const endDate = moment(row.enddate, 'MM/DD/YYYY').format('dddd, MMMM Do YYYY');
   const timeRange = `${startDate} ${row.starttime} - ${endDate} ${row.endtime}`;
 
   return {
-    addressRange,
+    addressRange: row.address,
     timeRange,
   } as ITowZone;
 };
 
 const checker = (address: IAddressConfig) => (onSuccess: Function, onError: Function) => {
   const consumer = new soda.Consumer('data.sfgov.org');
-  const nowStr = moment().format('YYYY-MM-DDTHH:mm:ss');
+  const nowStr = moment().format('MM/DD/YYYY');
   const streetNumber = parseInt(address.streetNumber, 10);
 
+  // https://data.sfgov.org/Transportation/SFMTA-Enforced-Temporary-Tow-Zones/9dsr-3f97
   consumer.query()
-    .withDataset('cqn5-muyy')
-    .where(soda.expr.gte('ending_date', nowStr))
-    .where({
-      streetname: address.streetName,
-    })
-    // .order('namelast')
+    .withDataset('89jz-v4ye')
+    .where(soda.expr.gte('EndDate', nowStr))
+    .where(`address like '%${address.streetName}%'`)
     .getRows()
     .on('success', (rows: Array<ISFGovTowZone>) => {
-      // street number ranges are strings from the api, so they can't be properly
+      // street number ranges are part of address in the api, so they can't be properly
       // queried on.
       const processRow = (memo: Array<Object>, row: ISFGovTowZone) => {
-        const toAddress = parseInt(row.toaddress, 10);
-        const fromAddress = parseInt(row.fromaddress, 10);
+        // this parsing is super fragile but the data is in the format '10 - 50 Main St'
+        const addressPieces = row.address.split(' - ');
+        const fromAddress = parseInt(addressPieces[0], 10);
+        const toAddress = parseInt(addressPieces[1].split(' ')[0], 10);
+
         if (fromAddress <= (streetNumber + 50) && toAddress >= (streetNumber - 50)) {
           memo.push(parseRow(row));
         }
